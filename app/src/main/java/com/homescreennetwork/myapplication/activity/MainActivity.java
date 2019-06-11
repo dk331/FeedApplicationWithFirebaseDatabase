76da28dfac6cae63abf909d40b355578e72ea474
Dhananjay Kulkarni <dhananjay.kulkarni@ps3g.com>
2019-06-11 17:57:32 +0530

    Initial commit

---
+++ app/src/main/java/com/homescreennetwork/myapplication/activity/MainActivity.java
@@ -0,0 +1,95 @@
+package com.homescreennetwork.myapplication.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import com.google.android.material.tabs.TabLayout;
+import com.google.firebase.auth.FirebaseAuth;
+import com.homescreennetwork.myapplication.R;
+import com.homescreennetwork.myapplication.fragment.MyPostsFragment;
+import com.homescreennetwork.myapplication.fragment.MyTopPostsFragment;
+import com.homescreennetwork.myapplication.fragment.RecentPostsFragment;
+
+public class MainActivity extends BaseActivity {
+
+    private static final String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        setTitle("Posts Feed");
+
+        // Create the adapter that will return a fragment for each section
+        FragmentPagerAdapter mPagerAdapter = new FragmentPagerAdapter(getSupportFragmentManager()) {
+            private final Fragment[] mFragments = new Fragment[]{
+                    new RecentPostsFragment(),
+                    new MyPostsFragment(),
+                    new MyTopPostsFragment(),
+            };
+            private final String[] mFragmentNames = new String[]{
+                    getString(R.string.heading_recent),
+                    getString(R.string.heading_my_posts),
+                    getString(R.string.heading_my_top_posts)
+            };
+
+            @NonNull
+            @Override
+            public Fragment getItem(int position) {
+                return mFragments[position];
+            }
+
+            @Override
+            public int getCount() {
+                return mFragments.length;
+            }
+
+            @Override
+            public CharSequence getPageTitle(int position) {
+                return mFragmentNames[position];
+            }
+        };
+        // Set up the ViewPager with the sections adapter.
+        ViewPager mViewPager = findViewById(R.id.container);
+        mViewPager.setAdapter(mPagerAdapter);
+        TabLayout tabLayout = findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(mViewPager);
+
+        // Button launches NewPostActivity
+        findViewById(R.id.fabNewPost).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, NewPostActivity.class));
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int i = item.getItemId();
+        if (i == R.id.action_logout) {
+            FirebaseAuth.getInstance().signOut();
+            startActivity(new Intent(this, SignInActivity.class));
+            finish();
+            return true;
+        } else {
+            return super.onOptionsItemSelected(item);
+        }
+    }
+
+}
---
+++ app/src/main/java/com/homescreennetwork/myapplication/fragment/RecentPostsFragment.java
@@ -0,0 +1,19 @@
+package com.homescreennetwork.myapplication.fragment;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.Query;
+
+public class RecentPostsFragment extends PostListFragment {
+
+    public RecentPostsFragment() {
+    }
+
+    @Override
+    public Query getQuery(DatabaseReference databaseReference) {
+        // [START recent_posts_query]
+        Query recentPostsQuery = databaseReference.child("posts");
+        // [END recent_posts_query]
+
+        return recentPostsQuery;
+    }
+}
